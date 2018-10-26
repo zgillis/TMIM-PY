0ea1bfe10a51ac0fc9753fc391cb13529eb76375
zgillis <zgillis11@gmail.com>
2018-10-26 14:07:15 -0500

    Update create_ddl.sql

--- create_ddl.sql
+++ create_ddl.sql
@@ -1,9 +1,10 @@
 CREATE TABLE users (
-  UID CHAR(9) PRIMARY KEY,
-  first_name VARCHAR(25) NOT NULL,
-  last_name VARCHAR(25) NOT NULL,
-  pwr_lvl INT(5) NOT NULL DEFAULT 1,
-  like_bal INT(10) NOT NULL DEFAULT 0,
-  wager_uid CHAR(9),
-  wager_likes INT(10)
+  UID CHAR(9) PRIMARY KEY, 
+  first_name VARCHAR(25) NOT NULL, 
+  last_name VARCHAR(25) NOT NULL, 
+  pwr_lvl INT(5) NOT NULL DEFAULT 1, 
+  like_bal INT(10) NOT NULL DEFAULT 0, 
+  wager_uid CHAR(9), 
+  wager_likes INT(10), 
+  CONSTRAINT user_wager_fk FOREIGN KEY(wager_uid) REFERENCES users(UID)
 );
