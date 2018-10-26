74f70554c074f18caa5302c3284dc1fd820ed8a7
zgillis <zgillis11@gmail.com>
2018-10-26 14:26:51 -0500

    Update create_things_ddl.sql

--- create_things_ddl.sql
+++ create_things_ddl.sql
@@ -1,7 +1,7 @@
 CREATE TABLE things ( 
     thing_id INT(5) AUTO_INCREMENT PRIMARY KEY, 
     UID CHAR(9) UNIQUE, 
-    name VARCHAR(25) NOT NULL, 
+    name VARCHAR(100) NOT NULL, 
     pwr_lvl INT(5) NOT NULL DEFAULT 1, 
     like_bal INT(10) NOT NULL DEFAULT 0, 
     wager_uid CHAR(9), 
